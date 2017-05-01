eb82889f9876fb520c26dc88c2ca09c5cbe32e14
Roldão Rego Jr <roldaogjr@gmail.com>
2017-05-01 17:37:38 -0300

    Added login page

--- cbvadmin/sites.py
+++ cbvadmin/sites.py
@@ -1,17 +1,20 @@
 from __future__ import unicode_literals
 import six
 from django.apps import apps
-from django.core.urlresolvers import reverse
 from django.conf.urls import url, include
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.core.urlresolvers import reverse
 from menu import MenuItem
 from .options import BaseAdmin
 from .views.dashboard import Dashboard
+from .utils import get_setting
 
 
 class AdminSite(object):
     _registry = {}
     name = 'cbvadmin'
     title = 'CBVAdmin'
+    login_form = None
 
     def register(self, model_class, admin_class):
         admin = admin_class(model_class)
@@ -20,7 +23,11 @@ class AdminSite(object):
 
     def get_urls(self):
         dashboard_view = Dashboard.as_view(admin=BaseAdmin(site=self))
-        urls = [url(r'^$', dashboard_view, name='dashboard')]
+        urls = [
+            url(r'^$', dashboard_view, name='dashboard'),
+            url(r'^login$', self.login, name='login'),
+            url(r'^logout$', self.logout, name='logout')
+        ]
         for model, admin in self._registry.iteritems():
             model_name = (model._meta.app_label,
                           model._meta.model_name)
@@ -43,6 +50,24 @@ class AdminSite(object):
     @property
     def urls(self):
         return self.get_urls(), 'cbvadmin', self.name
+
+    def login(self, request):
+        THEME = get_setting('theme', 'materialize')
+        from django.contrib.auth.views import login
+        context = {}
+        if (REDIRECT_FIELD_NAME not in request.GET and
+                REDIRECT_FIELD_NAME not in request.POST):
+            context[REDIRECT_FIELD_NAME] = reverse('cbvadmin:dashboard')
+        kwargs = {
+            'extra_context': context,
+            'template_name': 'cbvadmin/%s/login.html' % THEME
+        }
+        request.current_app = self.name
+        return login(request, **kwargs)
+
+    def logout(self, request):
+        from django.contrib.auth.views import logout_then_login
+        return logout_then_login(request, login_url=reverse('cbvadmin:login'))
 
 
 site = AdminSite()
