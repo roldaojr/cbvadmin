97f38f553315be4812a4c822cb2cf1e444c111a7
Roldão Rego Jr <roldaogjr@gmail.com>
2017-06-05 10:22:46 -0300

    Register UserAdmin only if default user model

--- cbvadmin/cbvadmin.py
+++ cbvadmin/cbvadmin.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.models import User, Group
+from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .options import ModelAdmin
-from .decorators import register
+from .sites import site
 
 
-@register(User)
 class UserAdmin(ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'active')
 
@@ -15,6 +14,11 @@ class UserAdmin(ModelAdmin):
             return UserCreationForm
 
 
-@register(Group)
 class GroupAdmin(ModelAdmin):
     list_display = ('name',)
+
+
+if settings.AUTH_USER_MODEL == 'auth.User' and getattr(settings, 'CBVADMIN_REGISTER_USER', True):
+    from django.contrib.auth.models import User, Group
+    site.register(User, UserAdmin)
+    site.register(Group, GroupAdmin)
