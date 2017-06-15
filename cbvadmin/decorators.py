99a2c2e05cd02bb2bddbaab751d80b670fbd5e3b
Roldão Rego Jr <roldaogjr@gmail.com>
2017-06-15 11:42:44 -0300

    Fix for register decorator

--- cbvadmin/decorators.py
+++ cbvadmin/decorators.py
@@ -3,5 +3,6 @@ from .sites import site
 
 def register(model_class):
     def decorated(admin_class):
-        return site.register(model_class, admin_class)
+        site.register(model_class, admin_class)
+        return admin_class
     return decorated
