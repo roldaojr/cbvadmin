268850c57e92da53118bf0a06524c93e713a1d7c
Roldão Rego Jr <roldaogjr@gmail.com>
2017-06-23 12:01:46 -0300

    Add StaticField

---
+++ cbvadmin/fields.py
@@ -0,0 +1,78 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import FieldDoesNotExist
+
+
+def is_rel_field(name, model):
+    if hasattr(name, 'split') and name.find("__") > 0:
+        parts = name.split("__")
+        if parts[0] in model._meta.get_all_field_names():
+            return True
+    return False
+
+
+def lookup_field(name, obj, model_admin=None):
+    opts = obj._meta
+    try:
+        f = opts.get_field(name)
+    except FieldDoesNotExist:
+        # For non-field values, the value is either a method, property or
+        # returned via a callable.
+        if callable(name):
+            attr = name
+            value = attr(obj)
+        elif (model_admin is not None and hasattr(model_admin, name) and
+              name not in ('__str__', '__unicode__')):
+            attr = getattr(model_admin, name)
+            value = attr(obj)
+        else:
+            if is_rel_field(name, obj):
+                parts = name.split("__")
+                rel_name, sub_rel_name = parts[0], "__".join(parts[1:])
+                rel_obj = getattr(obj, rel_name)
+                if rel_obj is not None:
+                    return lookup_field(sub_rel_name, rel_obj, model_admin)
+            attr = getattr(obj, name)
+            if callable(attr):
+                value = attr()
+            else:
+                value = attr
+        f = None
+    else:
+        attr = None
+        value = getattr(obj, name)
+    return f, attr, value
+
+
+class ValueField(object):
+    def __init__(self, obj, field_name, filter_func=None):
+        try:
+            f, attr, value = lookup_field(field_name, obj)
+        except (AttributeError, ObjectDoesNotExist):
+            pass
+        self.object = obj
+        self.name = field_name
+        self.field = f
+        if f:
+            label = f.verbose_name
+        elif hasattr(attr, 'verbose_name'):
+            label = attr.verbose_name
+        else:
+            label = field_name
+        self.auto_id = 'id_%s' % field_name
+        self.label = label
+        self.value = value
+
+        if callable(filter_func):
+            self.text = filter_func(value)
+        else:
+            self.text = value
+
+
+def valuefield_for_formfield(field, filter_func=None):
+    value_field = ValueField(field.form.instance, field.name,
+                             filter_func=filter_func)
+    value_field.label = field.label
+    value_field.value = field.value
+    value_field.form = field.form
+    value_field.auto_id = 'id_%s' % field.form.add_prefix(field.name)
+    return value_field
--- cbvadmin/layout.py
+++ cbvadmin/layout.py
@@ -1,5 +1,8 @@
+from django.template.loader import render_to_string
 from crispy_forms.helper import FormHelper
+from crispy_forms.utils import flatatt
 from crispy_forms.layout import *
+from .fields import valuefield_for_formfield, ValueField
 
 
 class Row(Div):
@@ -8,3 +11,68 @@ class Row(Div):
 
 class Column(Div):
     css_class = 'col'
+
+
+class Button(Button):
+    """
+    Used to create a Submit input descriptor for the {% crispy %} template tag:
+    .. sourcecode:: python
+        button = Button('Button 1', 'Press Me!')
+    .. note:: The first argument is also slugified and turned into the
+    id for the button.
+    """
+    input_type = 'button'
+    field_classes = 'btn waves-effect waves-light'
+
+
+class Submit(Submit):
+    """
+    Used to create a Submit button descriptor for the {% crispy %}
+    template tag:
+    .. sourcecode:: python
+        submit = Submit('Search the Site', 'search this site')
+    .. note:: The first argument is also slugified and turned into the id for
+              the submit button.
+    """
+    input_type = 'submit'
+    field_classes = 'btn waves-effect waves-light'
+
+
+class StrictField(Field):
+    template = "%s/field.strict.html"
+
+
+def instance_getter(self):
+    return getattr(self.form.instance, self.name, None)
+
+
+class StaticField(Field):
+    template = "%s/field.static.html"
+
+    def __init__(self, *args, **kwargs):
+        self._filter_func = kwargs.pop('filter_func', None)
+        super(StaticField, self).__init__(*args, **kwargs)
+
+    def _lookup_field(self, field_name, form):
+        try:
+            return valuefield_for_formfield(
+                form[field_name], filter_func=self._filter_func)
+        except KeyError:
+            return ValueField(form.instance, field_name,
+                              filter_func=self._filter_func)
+
+    def render_field(self, field, context, template):
+        attrs = self.attrs
+        context.update({
+            'wrapper_class': self.wrapper_class,
+            'field': field,
+            'flat_attrs': flatatt(attrs if isinstance(attrs, dict) else {}),
+        })
+        return render_to_string(template, context)
+
+    def render(self, form, form_style, context, *args, **kwargs):
+        fields = [self._lookup_field(field, form) for field in self.fields]
+        template = self.get_template_name(kwargs.get('template_pack'))
+        context.update(kwargs.get('extra_context', {}))
+        return ''.join(self.render_field(field, context, template)
+                       for field in fields)
