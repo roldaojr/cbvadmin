a5d366637275807061cb1a285c0e1fa5cb39883b
Roldão Rego Jr <roldaogjr@gmail.com>
2018-05-27 12:10:35 -0300

    Menu with all children invisible hides the menu

--- demo/app/cbvadmin.py
+++ demo/app/cbvadmin.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals, absolute_import
 import cbvadmin
+from menu import MenuItem
 from .models import Category, Computer
 
 
@@ -12,3 +13,10 @@ class CategoryAdmin(cbvadmin.ModelAdmin):
 class ComputerAdmin(cbvadmin.ModelAdmin):
     list_display = ('name', 'category', 'active')
     filter_fields = ('name', 'active')
+
+    def get_menu(self):
+        menu = super(ComputerAdmin, self).get_menu()
+        return menu + [MenuItem(
+            'Invisible menu', '', submenu=False, children=[
+                MenuItem('Invisible Item', '', check=lambda r: False)
+            ])]
