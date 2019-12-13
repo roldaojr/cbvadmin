3e2beeb014c2d4ed7eb2b887f7e3df17e11b2b8f
Roldão Rego Jr <roldaogjr@gmail.com>
2019-12-13 19:31:28 -0300

    Fix bug in password change view

--- cbvadmin/views/user.py
+++ cbvadmin/views/user.py
@@ -1,3 +1,4 @@
+from django.shortcuts import reverse, redirect
 from django.contrib.auth import get_user_model
 from django.views.generic.edit import FormView
 from django.views.generic import UpdateView
@@ -19,6 +20,10 @@ class PasswordChange(LoginRequiredMixin, AdminTemplateMixin, FormMixin,
         kwargs = super(PasswordChange, self).get_form_kwargs()
         kwargs['user'] = self.request.user
         return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return  redirect(reverse(self.admin.urls['logout']))
 
 
 class PasswordReset(LoginRequiredMixin, AdminTemplateMixin, FormMixin,
